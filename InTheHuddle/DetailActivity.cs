c89380c7fabe6232a07a9304bf68673a8b53e19f
alisoncerny <alisoncerny@gmail.com>
2016-11-17 13:35:58 -0600

    added web view to team detail

---
+++ InTheHuddle/DetailActivity.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+
+namespace InTheHuddle
+{
+	[Activity(Label = "DetailActivity")]
+	public class DetailActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.TeamDetail);
+
+			WebView lWebView = (WebView)FindViewById(Resource.Id.webView1);
+			lWebView.LoadUrl("file:///android_asset/test.html");
+		}
+	}
+}
--- InTheHuddle/MainActivity.cs
+++ InTheHuddle/MainActivity.cs
@@ -1,13 +1,13 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Webkit;
 
 namespace InTheHuddle
 {
 	[Activity(Label = "InTheHuddle", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
@@ -15,12 +15,6 @@ namespace InTheHuddle
 
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
-
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
-
-			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
 		}
 	}
 }
