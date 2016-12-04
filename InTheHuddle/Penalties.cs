72017e828581d34d9e7583d686239d5072423b4e
ZChucky <administrator@Zacks-MacBook-Pro.local>
2016-12-04 13:57:12 -0600

    Edited CSS to have new styles
    I put some new styles into the CSS file and edited the HTML files to
    include them.
    
    I also got rid of some of the repeated namespaces in the Penalties.cs
    file.

--- InTheHuddle/Penalties.cs
+++ InTheHuddle/Penalties.cs
@@ -4,11 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Android.App;
 using Android.Content;
 using Android.OS;
